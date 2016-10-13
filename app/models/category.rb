ddf1e6d8733b0cd5b581933f315e27f2b1d31ed7
TShine <tuan.tta1991@gmail.com>
2016-10-13 10:55:47 +0700

    Create model, Relationships and Validates

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ActiveRecord::Base
+  has_many :products
+
+  validates :title_name, uniqueness: true, presence: true, length: {maximum: 100}
+  validates :description, length: {maximum: 200}
+
+end
---
+++ app/models/request.rb
@@ -0,0 +1,6 @@
+class Request < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title_name, presence: true, length: {maximum: 50}
+  validates :content, presence: true, length: {maximum: 300}
+end
