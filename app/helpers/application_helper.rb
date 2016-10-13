57ab4998fbf593b5fca49fc14fed85949fb4a707
TShine <tuan.tta1991@gmail.com>
2016-10-13 10:55:47 +0700

    static_pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title page_title = ""
+    base_title = t "framgia_food_and_drinks"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
